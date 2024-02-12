6dd25b850e6a9278ea86edb42d5dd5a459f51963
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-02-12 19:14:24 +0530

    Added Navbar Routing

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,14 +10,14 @@ function Navbar() {
     <div className="nav">
       <div className="nav-left">
         <FontAwesomeIcon className="fa-film-icon" icon={faFilm} />
-        <span>Catalog-Z</span>
+        <a href={"/"}>Catalog-Z</a>
       </div>
       <div className="nav-right">
       {/*  eslint-disable-next-line*/}
-        <a href={""} className="active">Photos</a>
-        <a href={""}>Videos</a>
-        <a href={""}>About</a>
-        <a href={""}>Contact</a>
+      <a href={"/photos"} className="active">Photos</a>
+        <a href={"/videos"}>Videos</a>
+        <a href={"/about"}>About</a>
+        <a href={"/contact"}>Contact</a>
       </div>
       <input type="checkbox"id="checkbox" />
         <label for="checkbox" className="check-btn">
